40f50cc31098ec1a8178e30471cd14f9c91a83e7
Gambadeur <blardonemathieu@yahoo.fr>
2017-04-03 17:55:00 +0200

    commit3

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
 var express = require ('express');
 var app = express();
+var users = require ('./users.js');
 
 
 // DECLARER LES ROUTES DU PLUS PRECIS AU PLUS GLOBALE
 
-app.get('/', function (req, res){
-    res.send('Hello World');
+// app.get('/', function (req, res){
+//     res.send('Hello World');
+// });
+
+// app.get('/about', function (req, res){
+//     res.send('about');
+// });
+
+// app.get('/doudou/*', (req, res) => {
+//     res.send('doudou');
+//     console.log('params:', req.params )
+// });
+
+// app.get('/*', function (req, res){
+//     res.send('Cette page n\'existe pas mec !');
+// });
+
+
+
+// https://github.com/simplonco/js-express-routes
+
+
+
+app.get('/users', (req, res) => {
+
+    const tabName = users.map(function(item){
+        console.log(item.firstName);
+        return item.firstName;
+    });
+
+    const tabName2 = tabName.join(", ")
+    res.send(tabName2);
+
 });
 
-app.get('/about', function (req, res){
-    res.send('about');
+app.get('/users/:id', function (req, res){
+
+    res.send('Bonjour ' + users[req.params.id].firstName);
 });
 
-app.get('/doudou/*', (req, res) => {
-    res.send('doudou');
-    console.log('params:', req.params )
+
+app.get('/', (req, res) =>{
+    res.send('HomePage bibou');
 });
 
-app.get('/*', function (req, res){
-    res.send('Cette page n\'existe pas mec !');
-});
 
 
 app.listen(5000, function(){
---
+++ users.js
@@ -0,0 +1,10 @@
+
+module.exports = [
+
+    {id: 0, firstName: 'Michou'},
+    {id: 1, firstName: 'Jean-loup'},
+    {id: 2, firstName: 'Barnabé'},
+    {id: 3, firstName: 'Patrick'},
+    {id: 4, firstName: 'Bernard'},
+
+]
